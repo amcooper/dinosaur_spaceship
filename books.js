a7c968adaa7febd8359357ba51959aa7d2c83913
Adam Cooper <amcooper@gmail.com>
2015-03-23 19:10:37 -0400

    Fixed the 'next page' link. Switched urls to heroku for hosting there.

--- books.js
+++ books.js
@@ -6,58 +6,60 @@ var http = require("http");
 var fs = require("fs");
 var server = http.createServer(function(req,res){
 
-var requrl = req.url;
-var urlArr = requrl.split("/");
-console.log(urlArr);
-if (urlArr[2] === "style.css"){
-  fs.readFile("style.css" , function(err,data){
-    var style = data.toString();
-    res.end(style);
-  });
-}else if(library[urlArr[1]]){
-
-  fs.readFile("index.html" , function(err,data1){
-
-    fs.readFile( library[urlArr[1]] , function(err,data2){
-      var paginatedFile = paginate(data2.toString() , urlArr[2]);
-      var result = data1.toString().replace("REPLACE", paginatedFile);
-      result = result.replace("pBack", "http://adam.princesspeach.nyc/" + urlArr[1] + "/" + (parseInt(urlArr[2]) - 1)+" ");
-
-      var book = data2.toString();
-      var lines = book.split("\n");
-      var totalPages = Math.floor(lines.length / 22) + 1;
-      if (urlArr[2] < totalPages) {
-        result = result.toString().replace("pNext", "http://adam.princesspeach.nyc/" + urlArr[1] + "/" + (parseInt(urlArr[2]) + 1));
-      } else {
-        result = result.toString().replace("pNext","");
-      }
-
-      // result = result.replace("pNext", "http://localhost:2000/" + urlArr[1] + "/" + (parseInt(urlArr[2]) + 1)+" ");
-      result = result.toString().replace("ii", urlArr[2]);
-      title = urlArr[1].replace(/_/g, " ");
-      result = result.replace("TITLE", title);
-      res.end(result);
+  var requrl = req.url;
+  var urlArr = requrl.split("/");
+  console.log(urlArr);
+  if (urlArr[2] === "style.css"){
+    fs.readFile("style.css" , function(err,data){
+      var style = data.toString();
+      res.end(style);
     });
-  });
-}
-else if (urlArr[1] === "favicon.ico") {
-  fs.readFile("favicon.ico", function(err, data) {
-    var icon = data.toString();
-    res.end(data);
-  });
-} else if (urlArr[1] === "splash.html") {
-  fs.readFile("splash.html", function(err, data) {
-    var splash = data.toString();
-    res.end(splash);
-  });
-} else if (urlArr[1] === "splash.css") {
-  fs.readFile("splash.css", function(err, data) {
-    var splashStyle = data.toString();
-    res.end(splashStyle);
-  });
-}
+  }else if(library[urlArr[1]]){
 
-});
+    fs.readFile("index.html" , function(err,data1){
+
+      fs.readFile( library[urlArr[1]] , function(err,data2){
+        var paginatedFile = paginate(data2.toString() , urlArr[2]);
+        var result = data1.toString().replace("REPLACE", paginatedFile);
+        result = result.replace("pBack", "https://dinosaur-spaceship.herokuapp.com/" + urlArr[1] + "/" + (parseInt(urlArr[2]) - 1)+" ");
+        // result = result.replace("pBack", "http://localhost:2000/" + urlArr[1] + "/" + (parseInt(urlArr[2]) - 1)+" ");
+
+        var book = data2.toString();
+        var lines = book.split("\n");
+        var totalPages = Math.floor(lines.length / 22) + 1;
+        if (urlArr[2] < totalPages) {
+          result = result.toString().replace("pNext", "https://dinosaur-spaceship.herokuapp.com/" + urlArr[1] + "/" + (parseInt(urlArr[2]) + 1));
+          // result = result.toString().replace("pNext", "http://localhost:2000/" + urlArr[1] + "/" + (parseInt(urlArr[2]) + 1));
+        } else {
+          result = result.toString().replace("pNext","");
+        }
+
+        // result = result.replace("pNext", "http://localhost:2000/" + urlArr[1] + "/" + (parseInt(urlArr[2]) + 1)+" ");
+        result = result.toString().replace("ii", urlArr[2]);
+        title = urlArr[1].replace(/_/g, " ");
+        result = result.replace("TITLE", title);
+        res.end(result);
+      });
+    });
+  }
+  else if (urlArr[1] === "favicon.ico") {
+    fs.readFile("favicon.ico", function(err, data) {
+      var icon = data.toString();
+      res.end(data);
+    });
+  } else if (urlArr[1] === "splash.html") {
+    fs.readFile("splash.html", function(err, data) {
+      var splash = data.toString();
+      res.end(splash);
+    });
+  } else if (urlArr[1] === "splash.css") {
+    fs.readFile("splash.css", function(err, data) {
+      var splashStyle = data.toString();
+      res.end(splashStyle);
+    });
+  }
+
+  });
 // server.listen(80);
 server.listen(2000);
 
